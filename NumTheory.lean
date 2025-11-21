dab5cd6e33c74bfd63b70157ced41337e148222c
ChemistMikeLam <43129403+ChemistMikeLam@users.noreply.github.com>
2025-11-21 18:49:34 +0800

    Add NumTheory.lean

---
+++ NumTheory.lean
@@ -0,0 +1,68 @@
+namespace NumTheory
+
+universe u
+variable
+  (a b c m n p x y z : Nat)
+
+inductive Div : Nat → Nat → Prop
+  | intro : {x y : Nat} → (h : ∃ (k : Nat), x * k = y) → Div x y
+
+inductive Prime : Nat → Prop
+  | intro : {p : Nat} → {_ : p > 1} → (h : ∀ (n : Nat) {_ : 1 < n} {_ : n < p}, ¬Div n p) → Prime p
+
+theorem one_div_any : Div 1 x :=
+  ⟨⟨x, Nat.one_mul x⟩⟩
+
+theorem any_div_zero : Div x 0 :=
+  ⟨⟨0, Nat.mul_zero x⟩⟩
+
+theorem div_trans : Div x y → Div y z → Div x z := by
+  intro ⟨⟨kxy, hxy⟩⟩ ⟨⟨kyz, hyz⟩⟩
+  refine ⟨⟨kxy * kyz, ?_⟩⟩
+  rw [← Nat.mul_assoc, hxy, hyz]
+
+instance : Trans Div Div Div where
+  trans := λ {x y z : Nat} => div_trans x y z
+
+theorem not_div : {_ : b > a * x} → {_ : b < a * (x + 1)} → ¬Div a b := by
+  intro hs hl ⟨⟨k, hk⟩⟩
+  by_cases h : k ≥ x + 1
+  case pos =>
+    have cont : b < b := calc
+      b < a * (x + 1) := hl
+      _ ≤ a * k := Nat.mul_le_mul_left a h 
+      _ = b := hk
+    exact Nat.lt_irrefl b cont
+  case neg =>
+    have h' : k ≤ x := Nat.le_iff_lt_add_one.mpr $ Nat.not_le.mp h
+    have cont : b < b := calc
+      b = a * k := hk.symm
+      _ ≤ a * x := Nat.mul_le_mul_left a h'
+      _ < b := hs
+    exact Nat.lt_irrefl b cont
+
+theorem two_prime : Prime 2 := by
+  apply @Prime.intro 2 (by trivial)
+  intro
+  | 0 | 1 | _ + 2 => intros; contradiction
+
+theorem three_prime : Prime 3 := by
+  apply @Prime.intro 3 (by trivial)
+  intro
+  | 0 | 1 | _ + 3 => intros; contradiction
+  | 2 => simp; exact @not_div 2 3 1 (by trivial) (by trivial)
+
+theorem four_not_prime : ¬ Prime 4 :=
+  λ ⟨h⟩ => @h 2 (by trivial) (by trivial) ⟨⟨2, rfl⟩⟩
+
+theorem five_prime : Prime 5 := by
+  apply @Prime.intro 5 (by trivial)
+  intro
+  | 0 | 1 | _ + 5 => intros; contradiction
+  | n@h : 2 | n@h : 3 | n@h : 4 =>
+    simp
+    have h1 := @not_div n 5 (5/n) (by simp [h]) (by simp [h])
+    rw [h] at h1
+    exact h1
+
+end NumTheory
